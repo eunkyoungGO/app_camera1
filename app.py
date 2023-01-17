a0d88c219b20dd75b7c034732a63e5944d05c015
User <happy832674@gmail.com>
2023-01-17 23:38:41 +0900

    2023-01-17

--- app.py
+++ app.py
@@ -14,14 +14,3 @@ pic = st.camera_input("사진찍기")
 if pic:
     st.image(pic)
 
-    file_path = os.path.dirname(__file__)
-    pic_path = os.path.join(file_path, 'pics')
-
-    if not os.path.exists(pic_path):
-        os.mkdir(pic_path)
-
-if pic is not None:
-    pic.name = hakbun+name
-    fname, ext = os.path.splitext(pic.name)
-    with open(os.path.join(pic_path, fname+ext), 'wb') as f:
-        f.write(pic.getbuffer())
