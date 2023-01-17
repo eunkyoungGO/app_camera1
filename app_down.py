77581ead7384ff5fe1363a9ee28f1ba2cb41c948
User <happy832674@gmail.com>
2023-01-18 00:46:07 +0900

    2023-01-17

--- app_down.py
+++ app_down.py
@@ -6,19 +6,3 @@ col1, col2 = st.columns(2)
 
 hakbun = col1.text_input("학번")
 name = col2.text_input("이름")
-
-if pic:
-    st.image(pic)
-
-
-file_path = os.path.dirname(__file__)
-pic_path = os.path.join(file_path, 'pics')
-
-if not os.path.exists(pic_path):
-    os.mkdir(pic_path)
-
-if pic is not None:
-    pic.name = hakbun+name
-    fname, ext = os.path.splitext(pic.name)
-    with open(os.path.join(pic_path, fname+ext), 'wb') as f:
-        f.write(pic.getbuffer())
