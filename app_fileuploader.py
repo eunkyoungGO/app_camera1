0e5b3271513b9c7b3aa527abf68cc29c1d8e8560
User <happy832674@gmail.com>
2023-01-17 15:02:08 +0900

    2023-01-17

--- app_fileuploader.py
+++ app_fileuploader.py
@@ -5,10 +5,11 @@ pic = st.camera_input("사진찍기")
 
 if pic:
     st.image(pic)
-    img_path=os.path.join(os.path.dirname(__file__),'images')
 
-if not os.path.exists("img_path"):
-    os.mkdir("img_path")
+img_path=os.path.join(os.path.dirname(__file__),'images')
+
+if not os.path.exists(img_path):
+    os.mkdir(img_path)
 
 uploaded_file=st.file_uploader("이미지선택", type=['jpg','png'])
 
