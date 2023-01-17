34089b5f29f88a1cb58bf6accd63cb18002d77f8
User <happy832674@gmail.com>
2023-01-17 15:11:29 +0900

    2023-01-17

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ name = col2.text_input("이름")
 
 pic = st.camera_input("사진찍기")
 
-img_path=os.path.join(os.path.dirname(__file__),'images')
-uploaded_file=st.file_uploader("이미지선택", type=['jpg','png'])
+img_path = os.path.join(os.path.dirname(__file__), 'images')
+uploaded_file = st.file_uploader("이미지선택", type=['jpg', 'png'])
 
 if pic is not None:
     img_name = hakbun+name
     fname, ext = os.path.splitext(pic.name)
-    with open(os.path.join(img_path, img_name+ext),'wb') as f:
+    with open(os.path.join(img_path, img_name+ext), 'wb') as f:
         f.write(uploaded_file.getbuffer())
