6765208e0ae110c0af718fe5fa6c3b286c1664d3
User <happy832674@gmail.com>
2023-01-18 00:54:20 +0900

    2023-01-17

--- app.py
+++ app.py
@@ -14,7 +14,13 @@ if pic:
     st.image(pic)
 
 file_path = os.path.dirname(__file__)
-pic_path = os.path.join(file_path, 'pics')
+pic_path = os.path.join(file_path)
 
 if not os.path.exists(pic_path):
     os.mkdir(pic_path)
+
+if pic is not None:
+    pic.name = hakbun+name
+    fname, ext = os.path.splitext(pic.name)
+    with open(os.path.join(pic_path, fname+ext), 'wb') as f:
+        f.write(pic.getbuffer())
